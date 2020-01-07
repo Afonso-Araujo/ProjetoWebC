eeb23a21d2b580703bb880679bc4f2e802a3b38d
Alex <2180643@my.ipleiria.pt>
2020-01-07 16:49:25 +0000

    api working

--- js/api.js
+++ js/api.js
@@ -9,9 +9,7 @@ const URL_Multi = "http://api.openweathermap.org/data/2.5/group?";
 // q=London,uk - Cidade
 // &appid=b6907d289e10d714a6e88b30761fae22 - API_KEY
 
-var div_canada_clone = null;
-var div_lisboa_clone = null;
-var div_tokyo_clone = null;
+var div_cidades_clone = null;
 
 function paises_iniciais() {
  /*
@@ -123,63 +121,56 @@ function paises_iniciais() {
 
     canada(pais_canada);
     lisboa(pais_lisboa);
-    tokyo(pais_tokyo);*/
+    tokyo(pais_tokyo);
+*/
 
     $.ajax({
         method: "GET",
         url: URL_Multi,
         data: {
             id: "6167865,1850147,2267057,5368361,703448,5128581",
+            units: "metric",
             appid: API_KEY
         },
     }).done(function (msg) {
-
-        var results = [msg]; // array dos elementos
-
-        var lista_filmes = $("#lista-filmes");
+        var lista_filmes = $("#lista-cidades");
         lista_filmes.html("");
-       console.log(results);
+    console.log(msg);
+        var i;
+        for(i = 0; i < msg.cnt; i++){
+            //console.log(msg.list[i].name);
 
-        results.forEach(function (result) {
+            var result_html = div_cidades_clone.clone();
 
-            var result_html = div_canada_clone.clone();
-            console.log(results["id"]);
-            var cidade = result["id"];
-         /*   var temperatuta = msg["main"]["temp"];
-            var pressao = msg["main"]["pressure"];
-            var humidade = msg["main"]["humidity"];
-            var vento = msg["wind"]["speed"];
-            var tempo = msg["weather"]["main"];
-            var tempo_descicao = msg["weather"]["main"];*/
+            var cidade = msg.list[i].name;
+            var temperatuta = msg.list[i].main.temp;
+            var vento = msg.list[i].wind.speed;
+            var pressao = msg.list[i].main.pressure;
+            var humidade = msg.list[i].main.humidity;
+            var tempo = msg.list[i].weather[0].main;
 
-            var cidade_html_canada = $(".cidade", result_html);
-    //        var temperatura_html_canada = $(".temperatura", result_canada);
+            var cidade_html = $(".cidade", result_html);
+            var temperatura_html = $(".temperatura",result_html);
+            var vento_html = $(".vento",result_html);
+            var pressao_html = $(".pressao",result_html);
+            var humidade_html = $(".humidade",result_html);
+            var tempo_html = $(".tempo",result_html);
 
-            console.log("" +cidade);
-            cidade_html_canada.text(cidade);
-    //        temperatura_html_canada.text(temperatuta);
-            // console.log(cidade);
+            cidade_html.text(cidade);
+            temperatura_html.text(temperatuta + "ºC");
+            vento_html.text("Velocidade do Vento: " + vento + " Kms");
+            pressao_html.text("Pressâo Atmosferica: " + pressao);
+            humidade_html.text("Humidade: " + humidade + "%");
+            tempo_html.text(tempo);
 
             lista_filmes.append(result_html);
-        });
+        }
     });
 }
 
-function div_iniciais(){
-    var div_canada = $(".canada");
-    div_canada_clone = div_canada.clone();
-    div_canada.hide();
-
-    var div_lisboa = $(".lisboa");
-    div_lisboa_clone = div_lisboa.clone();
-    //div_canada.hide();
-
-    var div_tokyo = $(".tokyo");
-    div_tokyo_clone = div_tokyo.clone();
-    //div_canada.hide();
-}
-
 $(function () {
-    div_iniciais();
+    var div_cidades = $(".pre_cidades");
+    div_cidades_clone = div_cidades.clone();
+    div_cidades.hide();
     paises_iniciais();
 });
